49598aec7f77375628342faed7edcc52c845dfe1
PtowaPL <adam.pietrzak15063@gmail.com>
2025-10-27 20:24:21 +0100

    projekt1_etap3

--- main.c
+++ main.c
@@ -3,16 +3,18 @@
 #include <math.h>
 #include <stdlib.h>
 #include <locale.h>
+#include <time.h>
 
-void wspolczynniki(float* dane, int * ilosc_probek) {
+void wspolczynniki(float* dane, float* rozdzielczosc) {
 	printf("Podaj wspolczynnik A: ");
 	scanf("%f", &dane[0]);
 	printf("Podaj wspolczynnik B: ");
 	scanf("%f", &dane[1]);
 	printf("Podaj wspolczynnik C: ");
 	scanf("%f", &dane[2]);
-	printf("Podaj ilosc probek: ");
-	scanf("%d", ilosc_probek);
+	printf("Podaj rozdzielczosc: ");
+	scanf("%f", rozdzielczosc);
+
 
 }
 
@@ -23,37 +25,108 @@ void granice(float* xmin, float* xmax) {
 	scanf("%f", xmax);
 }
 
-void generator(float* sygnal, float* dane, float xmin, float xmax, int ilosc_probek) {
-	float deltax;
-	deltax = (xmax - xmin) / ilosc_probek;
+void generator(float* sygnal, float* dane, float xmin, float xmax, float rozdzielczosc, int probki) {
 
-	for (int i = 0; i < ilosc_probek; i++) {
-		sygnal[i] = dane[0] * sin((xmin + i * deltax) / dane[0]) + dane[1] * cos((xmin + i * deltax) / dane[1]) + dane[2];
+	for (int i = 0; i < probki; i++) {
+		sygnal[i] = dane[0] * sin((xmin + i * rozdzielczosc) / dane[0]) + dane[1] * cos((xmin + i * rozdzielczosc) / dane[1]) + dane[2];
 	}
 
 }
 
-void zapis(float* sygnal, int ilosc_probek) {
-	FILE* plik;
-	plik = fopen("sygnal.csv", "w");
-	for (int i = 0; i < ilosc_probek; i++) {
-		fprintf(plik, "%f\n", sygnal[i]);
+
+void naprawa(float xmin, float xmax, float rozdzielczosc, int* probki) {
+	*probki = (xmax - xmin) / rozdzielczosc;
+}
+
+void szum(float* sygnal, float* sygnal_szum, int probki, float* wspolczynniki) {
+	float amplituda_szum;
+	int zmienna;
+	printf("Podaj amplitude szumu: ");
+	scanf("%f", &amplituda_szum);
+
+	for (int i = 0; i < probki; i++) {
+		zmienna = rand() % 10 + 1;
+		if (zmienna < 4) {
+			sygnal_szum[i] = sygnal[i] + amplituda_szum;
+		}
+		else
+			sygnal_szum[i] = sygnal[i];
+
 	}
-	fclose(plik);
+}
+
+
+void zapis(float* sygnal, float* sygnal_szum, int ilosc_probek) {
+	if (sygnal_szum == NULL) {
+		FILE* plik;
+		plik = fopen("sygnal.csv", "w");
+		if (plik == NULL)
+		{
+			printf("blad");
+			return;
+		}
+		for (int i = 0; i < ilosc_probek; i++) {
+			fprintf(plik, "%f\n", sygnal[i]);
+		}
+		fclose(plik);
+	}
+	else {
+		FILE* plik;
+		plik = fopen("sygnal.csv", "w");
+		if (plik == NULL)
+		{
+			printf("blad");
+			return;
+		}
+		for (int i = 0; i < ilosc_probek; i++) {
+			fprintf(plik, "%f;%f\n", sygnal[i], sygnal_szum[i]);
+		}
+		fclose(plik);
+	}
 }
 
 int main() {
 	setlocale(LC_ALL, "polish_poland");
+	srand(time(NULL));
 	float dane[3];
 	float xmin, xmax;
-	float* sygnal;
-	int n;
+	float* sygnal = NULL;
+	float* sygnal_szum = NULL;
+	float n;
+	int probki = 0;
+	int menu = -1;
+	while (menu != 4) {
+		printf("Wybierz opcje:\n 1. Generowanie sygna³u podstawowego\n 2. Wprowadzanie szumu do sygna³u\n 3. Zapis do pliku\n 4.Zakoñczenie programu\n");
+		scanf("%d", &menu);
+		switch (menu) {
+		case 1:
+			wspolczynniki(dane, &n);
+			granice(&xmin, &xmax);
+			naprawa(xmin, xmax, n, &probki);
+			sygnal = malloc(probki * sizeof(float));
+			generator(sygnal, dane, xmin, xmax, n, probki);
+			printf("Wygenerowano sygnal\n");
+			break;
+		case 2:
+			sygnal_szum = malloc(probki * sizeof(float));
+			szum(sygnal, sygnal_szum, probki, dane);
+			printf("Wprowadzono szum do sygnalu\n");
+			break;
+		case 3:
+			zapis(sygnal, sygnal_szum, probki);
+			printf("Zapisano do pliku\n");
+			break;
+		case 4:
+			printf("Zakoñczenie programu\n");
+			break;
+		default:
+			printf("Nieprawid³owa opcja, spróbuj ponownie.\n");
+		}
+	}
 
-	wspolczynniki(dane, &n);
-	sygnal = malloc(n * sizeof(float));
-	granice(&xmin, &xmax);
-	generator(sygnal, dane, xmin, xmax, n);
-	zapis(sygnal, n);
+	
+	
 	free(sygnal);
+	free(sygnal_szum);
 	return 0;
 }
